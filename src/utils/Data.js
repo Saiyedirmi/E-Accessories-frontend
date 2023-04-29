091cc79d2395265cf2bc6dfa38cdca334cfab9e7
Saiyedirmi <89587686+Saiyedirmi@users.noreply.github.com>
2023-04-29 09:23:35 +0530

    Add files via upload

---
+++ src/utils/Data.js
@@ -0,0 +1,29 @@
+const services = [
+{
+    title: "Free Shipping",
+    tagline: "From all orders above $100",
+    image: "images/service.png"
+},
+{
+    title: "Daily Surprise Offers",
+    tagline: "Save upto 25% off",
+    image: "images/service-02.png"
+},
+{
+    title: "Support 24/7",
+    tagline: "Shop with an expert",
+    image: "images/service-03.png"
+},
+{
+    title: "Affordable prices",
+    tagline: "Get Factory Default price",
+    image: "images/service-04.png"
+},
+{
+    title: "Secure Payments",
+    tagline: "100% Protected Payment",
+    image: "images/service-05.png"
+},
+
+
+];
